54974f7778df798b643091e9447965a4bf58eb2f
傅楼荣 <61681770@qq.com>
2016-06-07 17:50:18 +0800

    增加右键菜单，我的好友，我的司机，群组右键暂时没做，以及好友名片的一个样式面板，修改部分样式

---
+++ static/js/cssheight.js
@@ -0,0 +1,8 @@
+/**
+ * Created by flr on 2016/6/7.
+ */
+var bodyheight=document.body.clientHeight;
+$(".chat01_title").css({height:bodyheight*0.08})
+$(".accordion-inner").css({height:bodyheight*0.45})
+$(".chat01_content:last").css({height:bodyheight*0.26})
+$("textarea").css({height:bodyheight*0.2})
